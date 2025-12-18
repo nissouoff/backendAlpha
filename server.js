c749aa7203cbae884e71908c18ef1caa2ee90206
nissouoff <156860993+nissouoff@users.noreply.github.com>
2025-12-18 01:58:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -54,12 +54,15 @@ app.use((req, res, next) => {
 
 /* ================== MAILER ================== */
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    pass: process.env.SMTP_PASS // APP PASSWORD
   }
 });
+
 
 /* ================== INIT DB ================== */
 async function initDatabase() {
